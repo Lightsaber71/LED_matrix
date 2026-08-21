9ce097b342fc4f62d589dcef4b918eba13c70768
Lightsaber71 <hokeypokey709@gmail.com>
2026-08-20 22:33:29 -0400

    feat: move fonts

---
+++ utils/paths.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+
+REPO_ROOT  = Path(__file__).parents[1]
+print(f"REPO_ROOT: {REPO_ROOT}")
+UTILS_DIR = REPO_ROOT / "utils"
+FONTS_DIR = UTILS_DIR / "fonts"
+
+
+
+
+# ======= Fonts ======= #
+font_4_x_6 = FONTS_DIR / "4x6.bdf"
+font_5_x_7 = FONTS_DIR / "5x7.bdf"
+font_5_x_8 = FONTS_DIR / "5x8.bdf"
+font_6_x_9 = FONTS_DIR / "6x9.bdf"
+font_6_x_10 = FONTS_DIR / "6x10.bdf"
+font_6_x_12 = FONTS_DIR / "6x12.bdf"
+font_6_x_13 = FONTS_DIR / "6x13.bdf"
+font_6_x_13B = FONTS_DIR / "6x13B.bdf"
+font_6_x_13O = FONTS_DIR / "6x13O.bdf"
+
+font_7_x_13 = FONTS_DIR / "7x13.bdf"
+font_7_x_13B = FONTS_DIR / "7x13B.bdf"
+font_7_x_13O = FONTS_DIR / "7x13O.bdf"
+font_7_x_14 = FONTS_DIR / "7x14.bdf"
+font_7_x_14B = FONTS_DIR / "7x14B.bdf"
+
+font_8_x_13 = FONTS_DIR / "8x13.bdf"
+font_8_x_13B = FONTS_DIR / "8x13B.bdf"
+font_8_x_13O = FONTS_DIR / "8x13O.bdf"
+
+font_9_x_15 = FONTS_DIR / "9x15.bdf"
+font_9_x_15B = FONTS_DIR / "9x15B.bdf"
+font_9_x_18 = FONTS_DIR / "9x18.bdf"
+font_9_x_18B = FONTS_DIR / "9x18B.bdf"
+
+font_10_x_20 = FONTS_DIR / "10x20.bdf"
+
+font_clR6_x_12 = FONTS_DIR / "clR6x12.bdf"
+font_helvR12 = FONTS_DIR / "helvR12.bdf"
+font_texgyre_27 = FONTS_DIR / "texgyre-27.bdf"
+font_tom_thumb = FONTS_DIR / "tom-thumb.bdf"
