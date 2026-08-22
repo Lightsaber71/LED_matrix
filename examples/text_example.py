2f78501653152f92492dc7ffdd80bd59a2cf110b
Lightsaber71 <hokeypokey709@gmail.com>
2026-08-21 22:55:54 -0400

    feat: add sun

--- examples/text_example.py
+++ examples/text_example.py
@@ -19,35 +19,86 @@ canvas = matrix
 red = graphics.Color(255, 0, 0)
 green = graphics.Color(0, 255, 0)
 blue = graphics.Color(0, 0, 255)
+hannahs_color = graphics.Color(87, 210, 255)
 
 # graphics.DrawLine(canvas, 5, 5, 22, 13, red)
 # graphics.DrawCircle(canvas, 15, 15, 10, green)
+
+
+def draw_sun(x, y, width, color):
+    radius = width // 2
+
+    # Sun
+    graphics.DrawCircle(
+        canvas,
+        y,
+        canvas.height - x - 1,
+        width // 2,
+        color
+    )
+
+    # Rays
+    ray_length = max(1, width // 4)
+    ray_distance = radius + 2
+    diag_ray_distance = radius
+
+    # Vertical rays
+    for i in range(1, ray_length + 1):
+        px(x, y - ray_distance - i + 1, color)
+        px(x, y + ray_distance + i - 1, color)
+
+    # Horizontal rays
+    for i in range(1, ray_length + 1):
+        px(x - ray_distance - i + 1, y, color)
+        px(x + ray_distance + i - 1, y, color)
+
+    # Diagonal rays
+    for i in range(1, ray_length + 1):
+        px(x - diag_ray_distance - i + 1, y - diag_ray_distance - i + 1, color)
+        px(x + diag_ray_distance + i - 1, y - diag_ray_distance - i + 1, color)
+        px(x - diag_ray_distance - i + 1, y + diag_ray_distance + i - 1, color)
+        px(x + diag_ray_distance + i - 1, y + diag_ray_distance + i - 1, color)
+
+        
+def px(x, y, color: graphics.Color):
+    if 0 <= x < 32 and 0 <= y < 64:
+        canvas.SetPixel(
+            y,
+            31 - x,
+            color.red,
+            color.green,
+            color.blue,
+        )
 
 font = graphics.Font()
 
 font.LoadFont(str(font_4_x_6))
 
-msg = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG"
+msg = "Hannah loves the sun... and tanning"
 
-graphics.DrawText(
-    canvas, 
-    font, 
-    2, 
-    font.height, 
-    red, 
-    msg,
-)
+# graphics.DrawText(
+#     canvas, 
+#     font, 
+#     2, 
+#     font.height, 
+#     red, 
+#     msg,
+# )
 
-time.sleep(10)
-canvas.Clear()
+# time.sleep(10)
+# canvas.Clear()
 
 printer = MatrixStringPrinter(
     canvas=canvas,
     font = font,
     col_pad = 0,
-    color=red,
+    color=hannahs_color,
 )
 printer.print_string(msg)
+
+draw_sun(10,40,10,graphics.Color(240, 199, 38))
+
+
 
 
 # graphics.DrawText(canvas, font, 2, 10, blue, "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG")
